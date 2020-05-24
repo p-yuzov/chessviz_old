c61dacac3dd9e27b1ff6883f36603ba90b15536c
p-yuzov <p.yuzov@gmail.com>
2020-05-24 22:12:17 +0700

    add method printBoard

--- chessviz/chessviz/chessviz.cpp
+++ chessviz/chessviz/chessviz.cpp
@@ -3,6 +3,23 @@
 
 #include <iostream>
 char board[8][8];
+char abcIdx[8] = {'a','b','c','d','e','f','g','h'};
+void printBoard()
+{
+    for (int i = 7; i >= 0; i--)
+    {
+        printf("\n%i", i + 1);
+        for (int j = 0; j < 8; j++)
+        {
+            printf("%c", board[i][j]);
+        }
+    }
+    printf("\n ");
+    for (int i = 0; i < 8; i++)
+    {
+        printf("%c", abcIdx[i]);
+    }
+}
 void initBoard()
 {
     board[0][0] = 'R';
@@ -41,4 +58,5 @@ void initBoard()
 int main()
 {
     initBoard();
+    printBoard();
 }
